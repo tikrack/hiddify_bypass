3645bfcefa1e23ebb2ac341c9977f76d99344c6a
tikrack <tikrackcode@gmail.com>
2025-08-06 16:03:52 +0330

    [Fetch] -> handle fetch url from query string

--- src/worker.js
+++ src/worker.js
@@ -1,6 +1,24 @@
 export default {
 	async fetch(request, env, ctx) {
-		console.log(env.URL)
+		let url = URL.parse(request.url);
+
+		let target = getUrl("url", url.search)
+
+		console.log(target);
+
+		// fetch(request, {
+		// 	method: 'GET',
+		//
+		// })
 		return new Response('Hello ddddWorld!');
 	}
 };
+
+const getUrl = (name, search) => {
+	search = search.substring(1)
+	search = search.split("=")
+
+	if (search[0] === name) {
+		return decodeURIComponent(search[1])
+	}
+}
